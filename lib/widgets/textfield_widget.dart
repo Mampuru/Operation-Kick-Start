d8f1796d23493ab34ba990fe405d3ca0ba1aa0a2
Mampuru <mampuruntwampe@gmail.com>
2023-11-30 14:56:48 +0200

    Updated the custom text widget with obscure field

--- lib/widgets/textfield_widget.dart
+++ lib/widgets/textfield_widget.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-class TextfieldWidget extends StatelessWidget {
+class TextFieldWidget extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final bool? obscureText;
 
-  const TextfieldWidget({
+  const TextFieldWidget({
     Key? key,
     required this.controller,
     required this.label,
+    this.obscureText,
   }) : super(key: key);
 
   @override
@@ -15,7 +17,8 @@ class TextfieldWidget extends StatelessWidget {
     keyboardType: TextInputType.multiline,
     textInputAction: TextInputAction.newline,
     minLines: 1,
-    maxLines: 5,
+    maxLines: obscureText != null ? 1:5,
+    obscureText: obscureText != null ? true : false,
     controller: controller,
     style: TextStyle(
       color: Colors.black,
