2cf9e5bcb31c5b6186fe5d3f573d55598bac7943
Mampuru <mampuruntwampe@gmail.com>
2023-12-05 21:56:02 +0200

    Addition of sign in controller

---
+++ lib/controllers/sign_in_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+class SignInController extends GetxController {
+  var email = ''.obs;
+  var password = ''.obs;
+
+  void setEmail(String value) => email.value = value;
+  void setPassword(String value) => password.value = value;
+
+  Future<void> sign() async {
+    // Implement your login logic here
+    // Example: Authenticate user with email and password
+    // Call API, validate credentials, etc.
+    try {
+      // Placeholder for login logic
+      // Check if email and password are valid
+      if (email.value == 'example@email.com' && password.value == 'password') {
+        // Successful login
+        // Navigate to the next screen or perform further actions
+        Get.toNamed('/home'); // Example: Navigate to home screen
+      } else {
+        // Invalid credentials
+        Get.snackbar('Error', 'Invalid email or password');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'An error occurred while logging in');
+    }
+  }
+}
