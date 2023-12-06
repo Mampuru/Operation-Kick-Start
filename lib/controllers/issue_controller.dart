c6e7fb44a971374998fc45ceabd9b60ec46ebb6b
Mampuru <mampuruntwampe@gmail.com>
2023-12-06 23:57:56 +0200

    Updated the issues controller with a fetch issues function

--- lib/controllers/issue_controller.dart
+++ lib/controllers/issue_controller.dart
@@ -5,6 +5,7 @@ class IssueController extends GetxController {
   RxString description = ''.obs;
   RxString location = ''.obs;
   Rx<String?> image = Rx<String?>(null);
+  RxList<String> issues = <String>[].obs;
 
   void setTitle(String value) => title.value = value;
   void setDescription(String value) => description.value = value;
@@ -33,5 +34,34 @@ class IssueController extends GetxController {
 
     // Show success message
     Get.snackbar('Success', 'Issue logged successfully');
+  }
+
+  // Function to fetch list of issues from API
+  Future<void> fetchIssues() async {
+    try {
+      // Placeholder: Replace this with your API endpoint for fetching issues
+      // Make API request to get a list of issues
+      List<String> fetchedIssues = await _fetchIssuesFromApi();
+
+      // Update the 'issues' list with the fetched data
+      issues.assignAll(fetchedIssues);
+    } catch (e) {
+      print('Error fetching issues: $e');
+      Get.snackbar('Error', 'Failed to fetch issues');
+    }
+  }
+
+  // Placeholder for API call (replace this with your actual API call logic)
+  Future<List<String>> _fetchIssuesFromApi() async {
+    // Simulate API call delay (replace this with your actual API request)
+    await Future.delayed(Duration(seconds: 2));
+
+    // Simulated list of fetched issues (replace this with your actual API response)
+    return [
+      'Issue 1',
+      'Issue 2',
+      'Issue 3',
+      // Add more issues as needed from the API response
+    ];
   }
 }
