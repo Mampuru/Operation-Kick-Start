4d1ba8ba856de285f6d53915cfdf2f630d59eed0
Mampuru <mampuruntwampe@gmail.com>
2024-03-24 00:19:00 +0200

    Fix the layout for sign up

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../contants.dart';
+import '../widgets/button_widget.dart';
 import '../widgets/textfield_widget.dart';
+import 'main_view.dart';
 
 class SignUpView extends StatefulWidget {
   const SignUpView({Key? key}) : super(key: key);
@@ -17,17 +20,66 @@ class _SignUpViewState extends State<SignUpView> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          TextFieldWidget(controller: name, label: 'Full Name',),
-          SizedBox(height: 10,),
-          TextFieldWidget(controller: email, label: 'Email',),
-          SizedBox(height: 10,),
-          TextFieldWidget(controller: password, label: 'Password',obscureText: true,),
-          SizedBox(height: 10,),
-          TextFieldWidget(controller: confirmPassword, label: 'Confirm Password',obscureText: true,),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 10.0),
+              Text(
+                "Welcome to SB365",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "Please Sign Up",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextFieldWidget(
+                controller: name,
+                label: 'Full Name',
+              ),
+              SizedBox(height: 20.0),
+              TextFieldWidget(
+                controller: email,
+                label: 'Email',
+              ),
+              SizedBox(height: 20.0),
+              TextFieldWidget(
+                controller: password,
+                label: 'Password',
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              TextFieldWidget(
+                controller: confirmPassword,
+                label: 'Confirm Password',
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              ButtonWidget(
+                buttonName: "Sign Up",
+                onTap: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainView()),
+                  )
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
