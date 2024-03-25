2f63d935cf74fa292a7e6e1770b0d16dfccd8fd7
Mampuru <mampuruntwampe@gmail.com>
2024-03-25 23:49:19 +0200

    Addition of the app version template class

---
+++ lib/widgets/app_version.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppVersion extends StatefulWidget {
+  const AppVersion({Key? key}) : super(key: key);
+
+  @override
+  State<AppVersion> createState() => _AppVersionState();
+}
+
+class _AppVersionState extends State<AppVersion> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
