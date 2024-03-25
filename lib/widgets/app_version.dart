a2ff5daea12dba085a6547948736b35fd57e7553
Mampuru <mampuruntwampe@gmail.com>
2024-03-25 23:55:41 +0200

    Updated the app version widget

--- lib/widgets/app_version.dart
+++ lib/widgets/app_version.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AppVersion extends StatefulWidget {
-  const AppVersion({Key? key}) : super(key: key);
+  final Color? color;
+  AppVersion({Key? key, this.color}) : super(key: key);
 
   @override
-  State<AppVersion> createState() => _AppVersionState();
+  _AppVersionState createState() => _AppVersionState();
 }
 
 class _AppVersionState extends State<AppVersion> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: <Widget>[
+        Text(
+          "v" + _packageInfo.version + "." + _packageInfo.buildNumber,
+          style: TextStyle(color: widget.color),
+        )
+      ],
+    );
   }
 }
